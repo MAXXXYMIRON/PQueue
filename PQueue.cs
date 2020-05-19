c2f9e46178612dc01795bce747120c2f880345ec
MAXXXYMIRON <klavioturasucka2@gmail.com>
2020-05-19 03:15:19 +0300

    pqueue0.0

---
+++ PQueue.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sort;
+
+namespace Program
+{
+    class PQueue<T>
+    {
+        /// <summary>
+        /// Список из очередей. Индексы, по которым находятся очереди, 
+        /// определяют приоритет, лежащих в ней элементов.
+        /// </summary>
+        List<Sort.Queue<T>> pqueue;
+        /// <summary>
+        /// Величина очереди и по совместительству величина наивысшего приоритета.
+        /// </summary>
+        int Length;
+        /// <summary>
+        /// Кол-во элементов в очереди.
+        /// </summary>
+        int CountOfPriority;
+
+        /// <summary>
+        /// Конструктор задает величину очереди приоритетов равную 50 
+        /// и размер очереди для данного приоретета равную 50. 
+        /// </summary>
+        public PQueue()
+        {
+            pqueue = new List<Sort.Queue<T>>();
+            Length = 50;
+            CountOfPriority = 50;
+        }
+        /// <summary>
+        /// Конструктор задает величину очереди приоритетов равную length 
+        /// и размер очереди для данного приоретета равную countOfPriority. 
+        /// </summary>
+        public PQueue(int countOfPriority, int length)
+        {
+            pqueue = new List<Sort.Queue<T>>();
+            Length = length;
+            CountOfPriority = countOfPriority;
+        }
+
+        /// <summary>
+        /// Количество элементов в очереди приоритетов.
+        /// </summary>
+        public int Count
+        {
+            get
+            {
+                int count = 0;
+
+                foreach (var item in pqueue)
+                {
+                    if (!(item is null || item.Count == 0))
+                        count += item.Count;
+                }
+
+                return count;
+            }
+        }
+
+        /// <summary>
+        /// Вернуть самый приоритетный элемент без удаления.
+        /// </summary>
+        public T PPeek()
+        {
+            if (pqueue.Count == 0) throw new Exception("Очередь приоритетов пуста");
+
+            return pqueue[pqueue.Count - 1].Peek();
+        }
+
+        /// <summary>
+        /// Вернуть самый приоритетный элемент с удалением.
+        /// </summary>
+        public T PDequeue()
+        {
+            if (pqueue.Count == 0) throw new Exception("Очередь приоритетов пуста");
+
+            T item = pqueue[pqueue.Count - 1].Dequeue();
+
+            while(pqueue[pqueue.Count - 1].IsEmpty)
+            {
+                pqueue.RemoveAt(pqueue.Count - 1);
+                if (pqueue.Count == 0) return item;
+            }
+            return item;
+        }
+
+        /// <summary>
+        /// Добавить элемент item, с приоритетом priority.
+        /// </summary>
+        public void PEnqueue(int priority, T item)
+        {
+            if (priority >= Length) throw new Exception("Большой приоритет, увеличте объем очереди.");
+            while (priority + 1 > pqueue.Count)
+            {
+                try
+                {
+                    pqueue.Add(new Sort.Queue<T>(CountOfPriority));
+                }
+                catch
+                {
+                    throw new Exception("Очередь данного приоритета переполнена.");
+                }
+            }
+            pqueue[priority].Enqueue(item);
+        }
+
+        /// <summary>
+        /// Проверить элемент на наличие в очереди приоритетов. 
+        /// </summary>
+        public bool Contains(T item)
+        {
+            foreach (var value in pqueue)
+            {
+                if (value.Contains(item)) return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Очистить очередь приоритетов. 
+        /// </summary>
+        public void Clear()
+        {
+            foreach (var item in pqueue)
+            {
+                item.Clear();
+            }
+            pqueue.Clear();
+        }
+
+        /// <summary>
+        /// Проверить пуста ли очередь приоритетов.
+        /// </summary>
+        public bool IsEmpty => (pqueue.Count == 0);
+
+        /// <summary>
+        /// Проверить плона ли очередь приоритетов.
+        /// </summary>
+        public bool IsFull => (pqueue.Count == Length);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    //Перечисление для приоритетов 
+    enum Employes { Manager, Supervisor, Worker };
+
+    //Структура работника
+    struct Work
+    {
+        public Employes Employ;
+        public int ID;
+        public int Time;
+
+        public Work(Employes employes, int id, int time)
+        {
+            Employ = employes;
+            ID = id;
+            Time = time;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Создадим массив с беспорядочным расположением сотрудников
+            Work[] empsRandom =
+            {
+                new Work(Employes.Manager,      100,    10),
+                new Work(Employes.Supervisor,   101,    20),
+                new Work(Employes.Worker,       102,    30),
+                new Work(Employes.Manager,      103,    40),
+                new Work(Employes.Supervisor,   104,    50),
+                new Work(Employes.Manager,      105,    10),
+                new Work(Employes.Manager,      106,    20),
+                new Work(Employes.Worker,       107,    30),
+                new Work(Employes.Supervisor,   108,    40),
+                new Work(Employes.Worker,       109,    50)
+            };
+
+            //Запишем данные в файл
+            Write(empsRandom);
+
+            //Запичем данные в очередь приоритетов
+            PQueue<Work> empsOrder = new PQueue<Work>();
+            Read(empsOrder);
+
+            //Запишем упорядоченные данные в файл
+            int[] summTime = new int[3];
+            Work item;
+            using (StreamWriter Write = new StreamWriter("OrderList.txt"))
+            {
+                Write.WriteLine("Должность\t\tНомер\tВремя\n");
+
+                while (!empsOrder.IsEmpty)
+                {
+                    item = empsOrder.PDequeue();
+
+                    summTime[(int)item.Employ] += item.Time;
+
+                    Write.WriteLine(Enum.GetName(item.Employ.GetType(), item.Employ).PadLeft(10)
+                         + item.ID.ToString().PadLeft(10) + item.Time.ToString().PadLeft(5)); 
+                }
+                
+                Write.WriteLine("\n");
+                Write.WriteLine($"Manager       {summTime[(int)Employes.Manager]}");
+                Write.WriteLine($"Worker        {summTime[(int)Employes.Worker]}");
+                Write.WriteLine($"Supervisor    {summTime[(int)Employes.Supervisor]}");
+            }
+        }
+
+
+        static void Write(Work[] emps)
+        {
+            FileInfo f = new FileInfo("work.txt");
+            using (BinaryWriter bw = new BinaryWriter(f.OpenWrite()))
+            {
+                for (int i = 0; i < emps.Length; i++)
+                {
+                    bw.Write((int)emps[i].Employ);
+                    bw.Write(emps[i].ID);
+                    bw.Write(emps[i].Time);
+                    bw.Write('\n');
+                }
+            }
+        }
+
+        static void Read(PQueue<Work> emps)
+        {
+            Work item = new Work(Employes.Manager, 0, 0);
+
+            FileInfo f = new FileInfo("work.txt");
+            using (BinaryReader br = new BinaryReader(f.OpenRead()))
+            {
+                while (br.BaseStream.Position < br.BaseStream.Length)
+                {
+                    item.Employ = (Employes)br.ReadInt32();
+                    item.ID = br.ReadInt32();
+                    item.Time = br.ReadInt32();
+                    br.BaseStream.Position++;
+
+                    emps.PEnqueue((int)item.Employ, item); 
+                }
+            }
+        }
+    }
+}
